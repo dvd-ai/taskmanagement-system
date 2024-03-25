3a07c57c767168bc95db4a8ee47b220995468f0d
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-03-25 17:41:00 +0100

    Initial commit

---
+++ src/main/java/com/interviewtask/taskmanagementsystem/TaskmanagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.interviewtask.taskmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementSystemApplication.class, args);
+	}
+
+}
